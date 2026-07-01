ace7fb6dd36e7c487716fb6be18f36e566b9a434
Saniya <saniyamihani031@gmail.com>
2026-07-02 00:55:29 +0530

    Add default Terraform variables

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,19 +1,23 @@
 variable "subscription_id" {
   description = "Azure Subscription ID"
   type        = string
+  default     = "fffbeb3f-c296-4399-b05d-dd812838c8cf"
 }
 
 variable "resource_group_name" {
-  description = "Azure Resource Group"
+  description = "Resource Group Name"
   type        = string
+  default     = "genai-gateway-rg"
 }
 
 variable "location" {
   description = "Azure Region"
   type        = string
+  default     = "eastus"
 }
 
 variable "environment" {
   description = "Deployment Environment"
   type        = string
+  default     = "staging"
 }
